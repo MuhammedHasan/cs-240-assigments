a6c28ebb0a16f4a6bea52aeb08a1820f9048399e
muhammed hasan celik <hasancelik@std.sehir.edu.tr>
2016-03-21 12:38:38 +0200

    pertange and score added

--- ass1/main.py
+++ ass1/main.py
@@ -8,36 +8,10 @@ from scipy import stats
 import random
 
 
-# TODO: add label for plot
-
 class PARTI:
 
     def __init__(self):
         self.df = PARTI._read_file()
-
-    @staticmethod
-    def _read_file():
-        '''
-            This static function readfiles and selects necessary rows.
-            return: pandas dataframe
-        '''
-        table = list()
-        f = open('wine.txt')
-        header = f.readline().strip().split('\t')[1:]
-        for i in f:
-            row = i.strip().split('\t')
-            if len(row) > 2:
-                table.append(map(int, row[1:]))
-            else:
-                break
-        return ps.DataFrame(table, columns=header)
-
-    def _get_hist_by_year(self, category, start_year, end_year):
-        years = {
-            (i / 12 + 1980): sum(self.df[category][i:i + 3])
-            for i in range(0, len(self.df[category]), 12)
-        }.items()
-        return filter(lambda x: end_year >= x[0] >= start_year, years)
 
     def plotHistogram(self):
         self.df.sum(axis=0).plot.bar()
@@ -64,6 +38,30 @@ class PARTI:
         )) / float(len(self.df[category]))
         return (vrn, math.sqrt(vrn))
 
+    @staticmethod
+    def _read_file():
+        '''
+            This static function readfiles and selects necessary rows.
+            return: pandas dataframe
+        '''
+        table = list()
+        f = open('wine.txt')
+        header = f.readline().strip().split('\t')[1:]
+        for i in f:
+            row = i.strip().split('\t')
+            if len(row) > 2:
+                table.append(map(int, row[1:]))
+            else:
+                break
+        return ps.DataFrame(table, columns=header)
+
+    def _get_hist_by_year(self, category, start_year, end_year):
+        years = {
+            (i / 12 + 1980): sum(self.df[category][i:i + 3])
+            for i in range(0, len(self.df[category]), 12)
+        }.items()
+        return filter(lambda x: end_year >= x[0] >= start_year, years)
+
 
 class PARTII:
 
@@ -73,24 +71,29 @@ class PARTII:
         tplot.Cdf(thinkstats2.MakeCdfFromPmf(pmf))
         tplot.Show()
 
-    def PMF(self):
-        X = [PARTII._choose_until_four() for i in range(10000)]
+    def PMF(self, list_of_int=range(1, 7)):
+        X = [PARTII._choose_until_four(list_of_int) for i in range(10000)]
         tplot.Hist(thinkstats2.Pmf(X))
         tplot.Show()
 
-    def CDF(self):
-        X = [PARTII._choose_until_four() for i in range(10000)]
+    def CDF(self, list_of_int=range(1, 7)):
+        X = [PARTII._choose_until_four(list_of_int) for i in range(10000)]
         tplot.Cdf(thinkstats2.Cdf(X))
         tplot.Show()
 
     def findDefective(self):
-        # TODO: For CDF, find percentiles and percentile ranks.
         books = ps.read_csv('Book2.csv')
         X = [PARTII._book_experiment(books) for i in range(100)]
         pmf = thinkstats2.Pmf(X)
         tplot.Pmf(pmf)
-        tplot.Cdf(thinkstats2.MakeCdfFromPmf(pmf))
+        cdf = thinkstats2.MakeCdfFromPmf(pmf)
+        tplot.Cdf(cdf)
         tplot.Show()
+
+        # percentiles and percentile ranks calculaition for all book.
+        ranks = map(cdf.PercentileRank, X)
+        pers = map(cdf.Percentile, ranks)
+        return (ranks, pers)
 
     @staticmethod
     def _book_experiment(books):
@@ -102,12 +105,12 @@ class PARTII:
         return num_try
 
     @staticmethod
-    def _choose_until_four():
+    def _choose_until_four(list_of_int):
         num_try = int()
         rand_choose_nums = list()
         while 4 not in rand_choose_nums:
             num_try += 1
-            rand_choose_nums.append(random.randint(1, 6))
+            rand_choose_nums.append(random.choice(list_of_int))
         return num_try
 
-print PARTII().findDefective()
+PARTII().findDefective()
